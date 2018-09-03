030bba79991847d08fbe61c7bd4aba2b48cb8000
Fedor Dmitriev <kscsq@mail.ru>
2018-09-03 15:55:08 +0300

    Apply 9_12_spring_security_test.patch

---
+++ src/main/webapp/resources/js/mealDatatables.js
@@ -0,0 +1,48 @@
+const ajaxUrl = "ajax/profile/meals/";
+let datatableApi;
+
+function updateTable() {
+    $.ajax({
+        type: "GET",
+        url: ajaxUrl + "filter",
+        data: $("#filter").serialize()
+    }).done(updateTableByData);
+}
+
+function clearFilter() {
+    $("#filter")[0].reset();
+    $.get(ajaxUrl, updateTableByData);
+}
+
+$(function () {
+    datatableApi = $("#datatable").DataTable({
+        "paging": false,
+        "info": true,
+        "columns": [
+            {
+                "data": "dateTime"
+            },
+            {
+                "data": "description"
+            },
+            {
+                "data": "calories"
+            },
+            {
+                "defaultContent": "Edit",
+                "orderable": false
+            },
+            {
+                "defaultContent": "Delete",
+                "orderable": false
+            }
+        ],
+        "order": [
+            [
+                0,
+                "desc"
+            ]
+        ]
+    });
+    makeEditable();
+});
